814db493ac97b5a3f93947b8848ac38fc55eed94
sant1dom <santone.domenico2k@gmail.com>
2023-07-02 10:20:48 +0200

    updated gitignores and other files

--- prosumer-1/src/main/java/it/disim/univaq/fantacalcio/controller/ProsumerController.java
+++ prosumer-1/src/main/java/it/disim/univaq/fantacalcio/controller/ProsumerController.java
@@ -1,5 +1,6 @@
 package it.disim.univaq.fantacalcio.controller;
 
+import io.swagger.annotations.ApiOperation;
 import it.disim.univaq.fantacalcio.endpoints.Lineups;
 import it.disim.univaq.fantacalcio.endpoints.LineupsImplService;
 import it.disim.univaq.fantacalcio.endpoints.Team;
@@ -7,8 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.xml.ws.Response;
 
 import java.util.List;
 
@@ -20,11 +19,16 @@ public class ProsumerController {
 	private String portNumber;
 
 	@GetMapping
+	@ApiOperation(value = "Get the best formation of all teams",
+			notes = "This operation returns the best formation of all teams")
 	public List<Team> getBestFormation() {
-		return lineupAllInvok();
+		System.out.println("\t\t [ProsumerController] - getBestFormation() invoked" + " on port " + portNumber);
+
+
+		return lineupAllInvoke();
 	}
 
-	private static List<Team> lineupAllInvok() {
+	private static List<Team> lineupAllInvoke() {
 		LineupsImplService service = new LineupsImplService();
 		Lineups port = service.getLineupsImplPort();
 		return port.lineupOfAllTeams();
