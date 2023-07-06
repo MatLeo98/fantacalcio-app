fffd8c09958ed0ff5d58f9f6c39b6c0c7aa6281c
MatLeo <leoncini.matteo98@gmail.com>
2023-07-06 17:35:02 +0200

    Update FantaClient: Feign e campioncini

--- fantaclient/src/main/java/it/disim/univaq/fantaclient/controller/FantaController.java
+++ fantaclient/src/main/java/it/disim/univaq/fantaclient/controller/FantaController.java
@@ -1,46 +1,46 @@
 package it.disim.univaq.fantaclient.controller;
 
-import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import it.disim.univaq.fantaclient.feignclient.FantaFeignClient;
 import it.disim.univaq.fantaclient.model.DataModel;
 
 @Controller
+@RequestMapping("/")
 public class FantaController {
 
+	@Autowired
+	private FantaFeignClient fantafeignclient;
     
-	@GetMapping("/")
+	@GetMapping
     public String home(Model model) {
         model.addAttribute("message", "The first Univaq fantasy football game!");
         return "home";
     }
-
-    @GetMapping("/best-formation")
-    public void sendGetRequest(Model model) {
-    	
-    	RestTemplate restTemplate = new RestTemplate();
-        String url = "http://host.docker.internal:9070/best-formation";
-        ResponseEntity<DataModel[]> best = restTemplate.getForEntity(url, DataModel[].class);
-        
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<String> roles = new ArrayList<>();
-        
-        for (DataModel player : best.getBody()) {
-        	names.add(player.getSurname());
-        	roles.add(player.getRole());
-        }
-        
-        ArrayList<String> response = new ArrayList<>();
-        for (int i = 0; i < roles.size(); i++) {
-            response.add(roles.get(i) + " - " + names.get(i));
-        }
-        
-        model.addAttribute("response", response);
-    }
+	
+	@GetMapping("/best-formation")
+	public void getBestFormation(Model model) {
+		List<DataModel> best = fantafeignclient.getBestFormation();
+		  var modulo = "";
+		  var dif = 0;
+		  var cen = 0;
+		  var att = 0;
+	      for(var giocatore:best) {
+	    	  switch(giocatore.getRole()) {
+		    	  case "D" -> dif += 1;
+		    	  case "C" -> cen += 1;
+		    	  case "A" -> att += 1;
+	    	  }
+	      }
+	      modulo = "" + dif + "-" + cen + "-" + att;	 
+	      model.addAttribute("modulo", modulo);
+	      model.addAttribute("response", best);
+	}
 	
 }
---
+++ fantaclient/src/main/java/it/disim/univaq/fantaclient/feignclient/FantaFeignClient.java
@@ -0,0 +1,14 @@
+package it.disim.univaq.fantaclient.feignclient;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import it.disim.univaq.fantaclient.model.DataModel;
+
+@FeignClient(name ="prosumer-1-microservice")
+public interface FantaFeignClient {
+	@GetMapping("/best-formation")
+	public List<DataModel> getBestFormation();
+}
